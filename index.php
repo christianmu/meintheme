a0b0a205c0feeff5083d711479f9cb543dd9cf36
Chrisian Musiol <musiol@post.cz>
2022-12-09 14:36:27 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Haupttemplate-Datei
+ *
+ * @package Aquila
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wordpress Theme</title>
+</head>
+
+<body>
+  Hallo Wordpress
+</body>
+
+</html>
